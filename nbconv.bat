33aa4a2f4ace1c2e87d2e099a5ff581ae2bbe0d0
Gene Asyura <gene@asyura.example.com>
2020-10-31 00:20:01 +0900

    Updated with latest data.

---
+++ nbconv.bat
@@ -0,0 +1,5 @@
+jupyter nbconvert tokyo.ipynb --to python
+jupyter nbconvert mhlw.ipynb --to python
+jupyter nbconvert prefectures.ipynb --to python
+jupyter nbconvert hokkaido.ipynb --to python
+jupyter nbconvert heatmap.ipynb --to python
--- run.bat
+++ run.bat
@@ -1,14 +1,5 @@
-jupyter nbconvert tokyo.ipynb --to python
 python tokyo.py
-
-jupyter nbconvert mhlw.ipynb --to python
 python mhlw.py
-
-jupyter nbconvert prefectures.ipynb --to python
 python prefectures.py
-
-jupyter nbconvert hokkaido.ipynb --to python
 python hokkaido.py
-
-jupyter nbconvert heatmap.ipynb --to python
 python heatmap.py
