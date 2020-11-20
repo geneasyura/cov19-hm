9ebe8ac2fb0dfffe977cdcf73b27b14561780ccc
Gene Asyura <gene@asyura.example.com>
2020-11-20 12:40:50 +0900

    bugfix.

--- air_registance.py
+++ air_registance.py
@@ -11,7 +11,7 @@ class AirRegistance:
         self.eta = None # 空気粘性率 [kg/ms]
         self.rho = None # 空気密度 [kg/m^3]
         self.v   = 0.80 # 球体速度 [m/s]
-        self.Re  = None # レイルズ数
+        self.re  = None # レイルズ数
         self.Cd  = None # 抗力係数
 
     def _calc_rho(self, t, P, M):
@@ -23,11 +23,14 @@ class AirRegistance:
         rho = (a*(P/(t+b))) * (1-M*c*((d*10**x)/(P)))
         return rho
 
-    def _calc_Cd(self, Re):
-        Cd = 24/Re + \
-            (26*(Re/5.0)) / (1+(Re/5.0)**1.52) + \
-            (0.411*(Re/263000)**(-7.94)) / (1+(Re/263000)**(-8)) + \
-            ((Re**0.80) / 461000)
+    def _calc_Cd(self, r):
+        #r = max(r, 1e-6
+        #if r < 0: 
+        #    return 0
+        Cd = 24/r + \
+            (26*(r/5.0)) / (1+(r/5.0)**1.52) + \
+            (0.411*(r/263000)**(-7.94)) / (1+(r/263000)**(-8)) + \
+            ((r**0.80) / 461000)
         return Cd
 
     def _calc_Fd(self):
@@ -40,9 +43,10 @@ class AirRegistance:
         self.eta = 1.487 * 1e-6 * ((K**1.5)/(K+117))
         self.rho = self._calc_rho(t, P, rh/100.0)
         #print("eta:{}, rho:{}".format(self.eta, self.rho))
-        self.Re = (self.v * self.rho * self.L) / self.eta
-        self.Cd = self._calc_Cd(self.Re)
-        #print("Re:{} Cd:{}".format(self.Re, self.Cd))
-        self.Fd = self._calc_Fd()
+        self.re = (self.v * self.rho * self.L) / self.eta
+        self.Cd = self._calc_Cd(self.re)
+        if self.re < 0:
+            pass#print("Re:{} Cd:{}, t:{}, P:{}, rh:{}".format(self.re, self.Cd, t, P, rh))
+        self.Fd = abs(self._calc_Fd())
         #print("Fd:{}".format(self.Fd))
         return self.Fd
